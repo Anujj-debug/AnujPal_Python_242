eece6f9a05b3abdfbf159c0f0d767896c119b793
AnujPal_Python_242 <anujworks2@gmail.com>
2025-08-27 09:35:00 +0530

    27 Aug

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 print("helllo")
 
+#camel case
+myVariableName = "hello"
+#snake case
+my_variable_name = "hello"
+#pascal case
+MyVariableName = "hello"
+
 z = (str)(123)
 print(z)
 print(type(z))
 
+#one value to multiple variables (it has same id)
 a = b = c = 10
 print(a)
 print(b)
@@ -12,9 +20,30 @@ print(id(a))
 print(id(b))
 print(id(c))
 
-#camel case
-myVariableName = "hello"
-#snake case
-my_variable_name = "hello"
-#pascal case
-MyVariableName = "hello"
+#many values to multiple variables (it has different id)
+x , y , z = "Orange" , "Banana" , "Cherry"
+print(id(x))
+print(id(y))
+print(id(z))
+
+#Unpacking a collection
+fruits = ["apple" , "banana" , "cherry"]
+x , y , z = fruits
+print(x)
+print(y)
+print(z)
+
+#Global variable
+x = "awesome"
+def myfunc():
+    print("Python is " + x)
+
+myfunc()    
+
+def myfunc2():
+    global g
+    g = "fantastic"
+
+myfunc2()
+
+print("Python is " + g)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def myfunc2():
+    global g
+    g = "fantastic"
+
+myfunc2()
+
+print("Python is " + g)
