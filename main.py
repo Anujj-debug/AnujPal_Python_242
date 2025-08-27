10505aeeacbedad28f6f1d0f193a632386f0a330
AnujPal_Python_242 <anujworks2@gmail.com>
2025-08-27 09:15:11 +0530

    another commit

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 print("helllo")
+
+z = (str)(123)
+print(z)
+print(type(z))
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+print(id(a))
+print(id(b))
+print(id(c))
+
+#camel case
+myVariableName = "hello"
+#snake case
+my_variable_name = "hello"
+#pascal case
+MyVariableName = "hello"
