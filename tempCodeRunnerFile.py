63c179527b5272e3cfeb500f08858dd0334ee60a
AnujPal_Python_242 <anujworks2@gmail.com>
2025-08-28 14:24:44 +0530

    added datatypes

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+var1 = 1
+var2 = True
+var3 = 10.023
+var4 = 10 + 3j
+
+print(id(var1))
+print(id(var2))
+print(id(var3))
+print(id(var4))
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
